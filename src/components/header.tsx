f89b5221532fd4ac6a7cfbe29aff7ef1b09f6d5c
Larry Saytee <saytee.software@web-developer.one>
2025-07-16 01:19:45 +0000

    MAKE THE Lightning Bolt Emoji ⚡a flat and white

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,16 @@ export const Header = () => {
           href="/"
           className="text-6xl md:text-8xl font-black tracking-tighter flex items-center justify-center"
         >
-          TRACK⚡BPM
+          TRACK
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-16 h-16 md:w-24 md:h-24 mx-1"
+          >
+            <path d="M11 21h-1l1-7H7l7-12h1l-1 7h4l-7 12z" />
+          </svg>
+          BPM
         </Link>
       </div>
     </header>
